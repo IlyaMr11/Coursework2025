ae4a99e90d98f304a24aa669f8880dc47c5e8a2c
Ilya Morozov <ilya112006m@yandex.ru>
2025-03-24 11:44:54 +0300

    create project

---
+++ Coursework2025/Coursework2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coursework2025
+//
+//  Created by Илья Морозов on 24.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
